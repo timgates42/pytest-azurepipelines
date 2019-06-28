a91684aed3f70acd8d37b2cee7e3681721d69574
Tim Gates <tim.gates@iress.com>
2019-06-29 08:26:17 +1000

    Add setuptools

--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -10,6 +10,6 @@ DEBIAN_FRONTEND=noninteractive apt-get update -qq
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends apt-utils
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends python3 python3-pip
 python3 -m pip install --upgrade pip
-python3 -m pip install pytest pytest-cov
-python3 setup.py install
+python3 -m pip install pytest pytest-cov setuptools
+python3 -m pip -e .
 python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
