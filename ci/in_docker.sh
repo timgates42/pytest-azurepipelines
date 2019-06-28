3eb4d640c952931ad44ad8fcba5f00db3728b3fc
Tim Gates <tim.gates@iress.com>
2019-06-29 07:28:22 +1000

    Stable docker install

--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -6,8 +6,9 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
 
 cd "${TOP}"
-apt-get -qq update
-apt-get -qq install -y python3 python3-pip
+apt-get update
+apt-get install -qq -y --no-install-recommends apt-utils
+apt-get install -qq -y --no-install-recommends python3 python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install -e .
 pytest tests -v -m "not testfail"
