77b0026f4473b05949054b2f1ad63e77c92dd508
Tim Gates <tim.gates@iress.com>
2019-06-29 08:21:27 +1000

    Ensure azure pipelines installed

--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -11,4 +11,5 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends ap
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends python3 python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install pytest pytest-cov
+python3 setup.py install
 python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
