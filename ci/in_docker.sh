25905db123937c696fdee186aa5b7c06ba85d932
Tim Gates <tim.gates@iress.com>
2019-06-29 07:34:38 +1000

    Stabilize docker install

--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -6,9 +6,9 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
 
 cd "${TOP}"
-apt-get update
-apt-get install -qq -y --no-install-recommends apt-utils
-apt-get install -qq -y --no-install-recommends python3 python3-pip
+DEBIAN_FRONTEND=noninteractive apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends apt-utils
+DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends python3 python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install -e .
 pytest tests -v -m "not testfail"
