3cb675733378edd7309a4d03f030afbd0a2e0b94
Tim Gates <tim.gates@iress.com>
2019-06-29 11:09:50 +1000

    Run install -e as local user

--- ci/docker/install.sh
+++ ci/docker/install.sh
@@ -11,4 +11,3 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends ap
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends python3 python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install pytest pytest-cov setuptools
-python3 -m pip install -e .
--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -4,4 +4,5 @@ set -euxo pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
+python3 -m pip install -e .
 python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
