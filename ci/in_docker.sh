b9d1f667212ee00ae784c42210d4eebc95b57923
Tim Gates <tim.gates@iress.com>
2019-06-29 11:20:47 +1000

    Move to correct directory

--- ci/in_docker.sh
+++ ci/in_docker.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$( dirname "${BASEDIR}" )"
+cd "${TOP}"
 python3 -m pip install -e .
 python3 -m pytest --cov=. --cov-report=xml -v -m "not testfail" tests
